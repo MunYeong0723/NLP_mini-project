7f70cb3cb569ca0cf16ea34c3cfe703ee2e7e7ac
MunYeong0723 <ansdud0723@naver.com>
2020-08-27 23:28:44 +0900

    [MunYeong0723] get movie list & users' comment

--- movie_crawling.py
+++ movie_crawling.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import csv
+import time
 
 def getMovies(movie_data) :
     headers = {
@@ -17,22 +18,34 @@ def getMovies(movie_data) :
         'sec-fetch-site': 'same-site',
         'sec-fetch-mode': 'cors',
         'sec-fetch-dest': 'empty',
-        'referer': 'https://pedia.watcha.com/ko-KR/staffmades/267',
+        'referer': 'https://pedia.watcha.com/ko-KR/staffmades/611',
         'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cookie': '_gid=GA1.2.304919113.1598425998; _c_drh=true; _ga_1PYHGTCRYW=GS1.1.1598426026.1.1.1598427123.0; _ga=GA1.2.1070950575.1598425998; _guinness_session=Yhk2q%2FrcUjzedlRQXbCro3DeB29wnKyf6FMF7473CaIv%2F%2B6m1EOt%2FvVDlfC3IHdFIGrHow%2B4PVCeZ%2BGYOi%2BF%2Faj9--zaAD1140tzEvv7Gh--zH4b8mkDVNn1J6Ve1eHR7A%3D%3D',
+        'cookie': '_gid=GA1.2.866190973.1598511669; _c_drh=true; _ga=GA1.1.1832522055.1598511669; _gat=1; _ga_1PYHGTCRYW=GS1.1.1598527836.3.1.1598535412.0; _guinness_session=V5fgQM%2FZNgn4nRplvV%2Bh5TyfksVfd9teX9S53poxZgphpgrwpz4VqP4cVfQo2aTqzNiUe7abv2Lv8dnIVGfz%2Fr%2BO--RbozoCRengK9MPan--BhbpVUH%2FfQAUySAK8u0kVQ%3D%3D',
+        'if-none-match': 'W/"c23b19f0180700f241bf6534e263ae70"',
     }
 
-    for page in range(1, 34):
+    movie_num = 0
+    for page in range(1, 10):
         params = (
             ('page', str(page)),
             ('size', '9'),
         )
 
-        response = requests.get('https://api-pedia.watcha.com/api/staffmades/267/contents', headers=headers, params=params)
+        response = requests.get('https://api-pedia.watcha.com/api/staffmades/611/contents', headers=headers, params=params)
+        if len(response.text) == 0 :
+            print("page : ", page)
+            continue
+
         js = json.loads(response.text)
+
         for movie in js['result']['result']:
             if movie['content_type'] != "movies":
                 continue
+
+            movie_num += 1
+            if movie_num % 5 == 0 :
+                print("wait 10 sec")
+                time.sleep(10)
 
             movie_one = {
                 'code' : movie['code'],
@@ -67,34 +80,31 @@ def getComment(movie_one):
         'sec-fetch-mode': 'cors',
         'sec-fetch-dest': 'empty',
         'referer': referer,
-        'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cookie': '_gid=GA1.2.866190973.1598511669; _c_drh=true; _gat=1; _guinness_session=9LFtKexXAEwfrulXaDIW4zvo8LN3%2BC3JJdqxSmZtm2TjaJ5eB45SPUNajgUnOJ2gLOJvtHhNmrg42ggGS1lFSGdu--okgGjiz8sVIXbpR5--S8w3Jxy2uV8ht8%2BWe1WHeg%3D%3D; _ga_1PYHGTCRYW=GS1.1.1598524754.2.1.1598525021.0; _ga=GA1.2.1832522055.1598511669',
+        'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
     }
 
     users = []
-    for i in range(1,20):
-        params = (
-            ('filter', 'all'),
-            ('order', 'popular'),
-            ('page', str(i)),
-            ('size', '20'),
-        )
+    params = (
+        ('filter', 'all'),
+        ('order', 'popular'),
+        ('page', '1'),
+        ('size', '10'),
+    )
 
-        response = requests.get(url, headers=headers, params=params)
-        js = json.loads(response.text)    
+    response = requests.get(url, headers=headers, params=params)
+    js = json.loads(response.text)
 
-        for c in js['result']['result']:
-            rt = c['user_content_action']['rating']
-            if rt is None :
-                rt = 0
+    for c in js['result']['result']:
+        rt = c['user_content_action']['rating']
+        if rt is None :
+            rt = 0
 
-            u = {
-                'user_rating' : round(rt/2,1),
-                'comment' : c['text'],
-                'user_code' : c['user']['code'],
-                'user_name' : c['user']['name']
-            }
-            users.append(u)
+        u = {
+            'user_code' : c['user']['code'],
+            'user_name' : c['user']['name'],
+            'user_rating' : round(rt/2,1)
+        }
+        users.append(u)
 
     movie_one['comments'] = users
     print(f"🙄 movie {movie_one['title']} get comments done ->", len(users))
@@ -107,27 +117,10 @@ def writeCSV(data, filename) :
         writer.writeheader()
         writer.writerows(data)
 
-def readCSV(filename, data):
-    with open(filename, 'r', encoding='utf-8-sig') as f :
-        rd = csv.reader(f)
-        keys = next(rd)
-        for rows in rd :
-            data.append({
-                keys[0] : rows[0],
-                keys[1] : rows[1]
-            })
-
 def main():
     movie_data = []
     getMovies(movie_data)
-    # writeCSV(movie_data, 'list_movie.csv')
-
-    # movie_code = []
-    # readCSV("list_movie.csv", movie_code)
-    # print(movie_code)
-
-    # users_list = []
-    # getComment(movie_code, users_list)
+    writeCSV(movie_data, 'list_movie2.csv')
 
 if __name__ == "__main__":
     main()
